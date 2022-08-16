0ea60ce3d0f8b3c5c75b8b07813950aeae25fd43
lucasroquesousa <lucasroquesousa@hotmail.com>
2022-08-16 10:14:31 -0300

    feat: estilização page sobre completa, faltando corrigir bug diplay dos cards

--- src/components/CardSobre/index.jsx
+++ src/components/CardSobre/index.jsx
@@ -1,7 +1,19 @@
+import { Box } from "@chakra-ui/react";
 import React from "react";
 
-const CardSobre = () => {
-  return <div>index</div>;
+const CardSobre = ({ children, ...rest }) => {
+  return (
+    <>
+      <Box
+        maxW={"310px"}
+        bg={"#D9D9D9"}
+        boxShadow={"0px 0px 8px #5c5c5c"}
+        {...rest}
+      >
+        {children}
+      </Box>
+    </>
+  );
 };
 
 export default CardSobre;
